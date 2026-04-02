ae7acc873b36509fbb00b57b3843cd8348ff713e
Carter Boyle <carter.boyle2@gmail.com>
2026-04-01 20:58:29 -0400

    Implement shooting prototype, still needs collisions

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED, SHOT_RADIUS
 import pygame
+
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -32,6 +34,12 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         self.position += pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position[0], self.position[1], SHOT_RADIUS) 
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
