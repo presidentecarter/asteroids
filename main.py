c55aae215bedd148cc2ee50ffd62966a3d05d36a
Carter Boyle <carter.boyle2@gmail.com>
2026-04-02 20:28:56 -0400

    make destruction of asteroids work

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for s in shots:
+                if a.collides_with(s):
+                    log_event("asteroid_shot")
+                    s.kill()
+                    a.kill()
         screen.fill("black")
         for drawing in drawable:
             drawing.draw(screen)
